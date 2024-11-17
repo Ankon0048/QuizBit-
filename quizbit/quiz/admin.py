115c2a58c6366a1abfa0bb8d42e7084855d948b3
Ankon48 <sergiokunankon@gmail.com>
2024-11-18 01:35:34 +0600

    Added Serializers to send response in JSON format. Completed all the tasks

--- quizbit/quiz/admin.py
+++ quizbit/quiz/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Users, Questions, History
 # Register your models here.
+
+
+# Registering the models in django administration for accessing the database
+admin.site.register(Users)
+admin.site.register(Questions)
+admin.site.register(History)
